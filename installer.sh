a08ec1c775b4184b15a5a31409a0e26fc607912b
Axistorm <gael.laguna@epitech.eu>
2025-05-23 19:00:52 +0200

    fix add model export to installer

--- installer.sh
+++ installer.sh
@@ -4,7 +4,11 @@ sudo apt install python3
 sudo apt install jupyter
 
 jupyter nbconvert --to script tardis_eda.ipynb
+jupyter nbconvert --to script tardis_model.ipynb
+
 python3 tardis_eda.py
+
+mv tardis_model.py dataset.py
 
 python3 -m venv .venv
 
